5b3fa46fbe54298555b7ea7cd536b3778dd92f83
Artyom Gurbovich <artyomgurbovich@gmail.com>
2019-10-04 21:53:13 +0300

    v2

--- OS Lab 3.cpp
+++ OS Lab 3.cpp
@@ -12,16 +12,30 @@
 #include <conio.h>
 using namespace std;
 
-#define BUFFER_SIZE 1024 * 256 // 256 килобайт
+#define BUFFER_SIZE 1024 * 256
 #define STATUS_OK 200
+#define FILTER_LENGTH 3
 
-int isRunning;
+string * getPathAndDomain(string inputString) {
+	string* returnString = new string[2];
+	string filter[FILTER_LENGTH] = { "https://", "http://", "www." };
+	size_t pos;
+	for (int i = 0; i < FILTER_LENGTH; i++) {
+		pos = inputString.find(filter[i]);
+		if (pos != string::npos) inputString.erase(pos, filter[i].length());
+	}
+	pos = inputString.find('/');
+	returnString[0] = inputString.substr(0, pos);
+	returnString[1] = inputString.substr(pos + 1);
+	return returnString;
+	delete[] returnString;
+}
 
-int parseHeader(int sock) {
+int getContentLength(int sock) {
 	char buff[BUFFER_SIZE], *ptr = buff + 4;
 	int bytes_received, status;
 	while (bytes_received = recv(sock, ptr, 1, 0)) {
-		if (bytes_received == -1) { puts("error"); return -1; }
+		if (bytes_received == -1) return -1;
 		if ((ptr[-3] == '\r') && (ptr[-2] == '\n') && (ptr[-1] == '\r') && (*ptr == '\n')) break;
 		ptr++;
 	}
@@ -35,11 +49,11 @@ int parseHeader(int sock) {
 	return bytes_received;
 }
 
-int readHttpStatus(int sock) {
+int getStatus(int sock) {
 	char buff[BUFFER_SIZE], *ptr = buff + 1;
 	int bytes_received, status;
 	while (bytes_received = recv(sock, ptr, 1, 0)) {
-		if (bytes_received == -1) { puts("errorS"); return -1; }
+		if (bytes_received == -1) return -1;
 		if ((ptr[-1] == '\r') && (*ptr == '\n')) break;
 		ptr++;
 	}
@@ -49,24 +63,36 @@ int readHttpStatus(int sock) {
 	return (bytes_received > 0) ? status : 0;
 }
 
-int downloadImage(int sock, int length) {
-	char buffer[BUFFER_SIZE];
+void donwloadImage(void* socket) {
+	int sock = (int)socket;
+	int contentLengh = getContentLength(sock);
 	int bytes = 0, bytes_received;
-	FILE* fd = fopen(string("test" + to_string(sock) + ".jpg").c_str(), "wb");
-	while ((bytes_received = recv(sock, buffer, BUFFER_SIZE, 0))) {
-		fwrite(buffer, 1, bytes_received, fd);
-		bytes += bytes_received;
-		if (bytes == length) break;
+	char buffer[BUFFER_SIZE];
+	if (contentLengh) {
+		FILE* fd = fopen(string("test" + to_string(sock) + ".jpg").c_str(), "wb");
+		while ((bytes_received = recv(sock, buffer, BUFFER_SIZE, 0))) {
+			fwrite(buffer, 1, bytes_received, fd);
+			bytes += bytes_received;
+			if (bytes == contentLengh) break;
+		}
+		fclose(fd);
+		closesocket(sock);
 	}
-	fclose(fd);
-	closesocket(sock);
-	return 0;
+	return;
 }
 
-void readInputAndSend() {
+int main() {
+
+	// "https://cdn.hasselblad.com/samples/x1d-II-50c/x1d-II-sample-01.jpg"
+	// "https://www.lifeofpix.com/wp-content/uploads/2019/10/DSC00873-1600x2396.jpg"
+	// "https://images.unsplash.com/photo-1565028832942-d005b1bd84f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+	// "https://images.pexels.com/photos/1955134/pexels-photo-1955134.jpeg?cs=srgb&dl=adventure-asphalt-daylight-1955134.jpg&fm=jpg"
+	// "https://image.freepik.com/free-photo/tropical-foliage-background-with-blank-card_24972-451.jpg"
+	// "https://cdn.pixabay.com/photo/2019/09/30/18/41/taxi-4516525_960_720.jpg"
+	// "http://www.effigis.com/wp-content/uploads/2015/02/Airbus_Pleiades_50cm_8bit_RGB_Yogyakarta.jpg"
+
 	char buffer[BUFFER_SIZE];
-	string url, domain_string, path_string;
-	size_t pos;
+	string url, *pathAndDomain;
 	const char *domain, *path;
 	int sock;
 	struct hostent *he;
@@ -74,54 +100,32 @@ void readInputAndSend() {
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(80);
 
-	while (isRunning) {
-		cout << "Enter image URL ('q' for exit): ";
-		getline(cin, url);
-		if (url == "q") isRunning = 0;
-		else {
-			pos = url.find('/');
-			domain_string = url.substr(0, pos);
-			path_string = url.substr(pos + 1);
-			domain = domain_string.c_str();
-			path = path_string.c_str();
-			try {
-				if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) throw string("socket");
-				if ((he = gethostbyname(domain)) == NULL) throw string("gethostbyname");
-				server_addr.sin_addr = *((struct in_addr *) he->h_addr);
-				if (connect(sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1) throw string("connect");
-				snprintf(buffer, sizeof(buffer), "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n", path, domain);
-				if (send(sock, buffer, strlen(buffer), 0) == -1) throw string("send");
-				if (readHttpStatus(sock) != STATUS_OK) throw string("readHttpStatus");
-				int contentLengh = parseHeader(sock);
-				cout << "contentLengh = " << contentLengh << endl;
-				if (contentLengh) {
-					thread t(downloadImage, sock, contentLengh);
-					t.detach();
-				}
-			}
-
-			catch (string error) {
-				cout << "Error in '" << error << "' function: " << WSAGetLastError() << endl;
-			}
-		}
-	}
-}
-
-int main() {
-	char buffer[BUFFER_SIZE];
-
 	if (WSAStartup(MAKEWORD(2, 2), (WSADATA *)&buffer[0])) {
 		cout << "Error in 'WSAStartup' function: " << WSAGetLastError() << endl;
 		return -1;
 	}
-	isRunning = 1;
-	thread t(readInputAndSend);
-	t.detach();
-	while (isRunning) {
-		Sleep(100);
+	while (true) {
+		cout << "Enter image URL ('q' for exit): ";
+		getline(cin, url);
+		if (url == "q") break;
+		pathAndDomain = getPathAndDomain(url);
+		domain = pathAndDomain[0].c_str();
+		path = pathAndDomain[1].c_str();
+		try {
+			if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) throw string("socket");
+			if ((he = gethostbyname(domain)) == NULL) throw string("gethostbyname");
+			server_addr.sin_addr = *((struct in_addr *) he->h_addr);
+			if (connect(sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1) throw string("connect");
+			snprintf(buffer, sizeof(buffer), "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n", path, domain);
+			if (send(sock, buffer, strlen(buffer), 0) == -1) throw string("send");
+			if (getStatus(sock) != STATUS_OK) throw string("readHttpStatus");
+			_beginthread(&donwloadImage, 0, (void *)sock);
+		}
+		catch (string error) {
+			cout << "Error in '" << error << "' function: " << WSAGetLastError() << endl;
+		}
 	}
-	WSACleanup();
 	cout << "Press any key to exit...";
-	getch();
+	_getch();
 	return 0;
 }
